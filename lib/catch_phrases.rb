8e7af42aaca1935feb9fb378ec2d7ea07d030d80
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-09-19 15:00:35 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,8 +5,9 @@ def mario
   puts phrase
 end
 
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
